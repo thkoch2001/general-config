20ef863f60736a497974ca359e87b00deaf797b3
Thomas Koch <thomas@koch.ro>
2012-09-10 19:48:10 +0200

    use x properties to run autoruns only once per xsession login

--- bin/run-autoruns.sh
+++ bin/run-autoruns.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
+PROP=autostart.wasrun
+HASPROP=$(xrdb -query|grep $PROP)
+
+if [ "$HASPROP" != "" ]
+then
+  exit 0;
+fi
+
 TEMPLATE=xsession-$USER-$PPID-XXX
 TEMPFILE=$(mktemp --tmpdir $TEMPLATE)
 
 echo "logging to $TEMPFILE"
+
+echo "$PROP: on" | xrdb -merge
 
 ( $HOME/bin/dex -v -a 2>&1 1>$TEMPFILE ) &
