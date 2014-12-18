4ddc2ee0f904d640a471e8fff23d70b949c2ca68
Thomas Koch <thomas@koch.ro>
2014-12-18 22:51:41 +0100

    solarized termit

--- .config/termit/rc.lua
+++ .config/termit/rc.lua
@@ -1,7 +1,33 @@
 defaults = {}
-defaults.foregroundColor = 'white'
-defaults.backgroundColor = 'black'
-defaults.font = 'Monospace 13'
+
+-- solarized color map from:
+-- https://gist.github.com/weakish/923868
+defaults.colormap = {
+'#070736364242',
+'#dcdc32322f2f',
+'#858599990000',
+'#b5b589890000',
+'#26268b8bd2d2',
+'#d3d336368282',
+'#2a2aa1a19898',
+'#eeeee8e8d5d5',
+'#00002b2b3636',
+'#cbcb4b4b1616',
+'#58586e6e7575',
+'#65657b7b8383',
+'#838394949696',
+'#6c6c7171c4c4',
+'#9393a1a1a1a1',
+'#fdfdf6f6e3e3',
+}
+
+-- more solarized termit links:
+-- https://github.com/alpha-omega/termite-colors-solarized
+-- https://github.com/altercation/solarized/issues/67
+
+-- defaults.foregroundColor = 'white'
+-- defaults.backgroundColor = 'black'
+defaults.font = 'Terminus 13'
 defaults.hideMenubar = true
 defaults.encoding = 'UTF-8'
 defaults.allowChangingTitle = true
